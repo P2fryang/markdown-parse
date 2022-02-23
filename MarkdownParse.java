a32289ed207f48f59a56f6943e64cbc46773da36
Frederick Yang <cs15lwi22aby@ieng6-202.ucsd.edu>
2022-02-23 15:26:53 -0800

    Stuff

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -19,7 +19,6 @@ public class MarkdownParse {
             } else if (markdown.charAt(closeParen) == ')') {
                 openParenCount--;
             }
-            closeParen++;
         }
         return closeParen - 1;
 
